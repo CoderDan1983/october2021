93e92ede3561c54ae1875e0e5d49be80aedff933
CoderDan1983 <seekingsomething121@gmail.com>
2021-10-15 10:33:54 -0700

    created class romneyfied

--- index.js
+++ index.js
@@ -3,3 +3,11 @@ function notMuchHasBeenDoneHere(){
 	const roll=round(Math.random());
 	return(roll);
 }
+
+class Romneyfied {
+	constructor(name, age, power){
+		this.name=name;
+		this.age=age;
+		this.power=power;
+	}
+}
