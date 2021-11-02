f7c990c7d2063773c8954acab0ed673d8a374a57
CoderDan1983 <seekingsomething121@gmail.com>
2021-11-02 06:26:17 -0700

    index changed!

--- index.js
+++ index.js
@@ -12,9 +12,14 @@ class Romneyfied {
 	}
 }
 
-let thisArray=[0,1,2,3,4,5,6,7];
+let thisArray=[0,1,2,3,4,5,6,7,8,9];
 let MC = {
 	bravery: 7,
 	strength: 5,
 	speed: 8
 }
+
+for (let property in MC){
+	console.log(`${property}: ${MC[property]}`);
+}
+
